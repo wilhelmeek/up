9ed018d8dae513472d903637ad60db011cb35301
Wilhelm <wilhelm.eek@gmail.com>
2020-07-25 11:55:28 +1000

    add gui lib

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/wilhelmeek/help
 
 go 1.14
+
+require (
+	github.com/gizak/termui/v3 v3.1.0 // indirect
+	github.com/jesseduffield/lazygit v0.20.9 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
+)
 
 func main() {
-  fmt.Printf("hello world\n")
+	if err := ui.Init(); err != nil {
+		log.Fatalf("failed to initialize termui: %v", err)
+	}
+	defer ui.Close()
+
+	p := widgets.NewParagraph()
+	p.Text = "Hey there!"
+	p.SetRect(0, 0, 25, 5)
+
+	ui.Render(p)
+
+	for e := range ui.PollEvents() {
+		if e.Type == ui.KeyboardEvent {
+			break
+		}
+	}
 }
